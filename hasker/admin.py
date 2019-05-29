0661fb3b6fad0fafb4cd8ff7f49656362d73bc31
yakotsar <yakotsar@yandex.ru>
2019-05-29 18:33:42 +0300

    models almost done

--- hasker/admin.py
+++ hasker/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Question, Answer, Tag
+from .models import Question, Answer, Tag, Profile
 
-'''
-@admin.register(Question, Answer, Tag)
-class HaskerAdmin(admin.ModelAdmin):
-    pass
-    '''
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Tag)
+admin.site.register(Profile)
--- hasker/models.py
+++ hasker/models.py
@@ -1,8 +1,16 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.db import models
+
+# https://docs.djangoproject.com/en/2.2/ref/models/fields/#django.db.models.FileField.upload_to
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Profile(User):
+    avatar = models.FileField(upload_to=user_directory_path)
 
 
 class Tag(models.Model):
@@ -11,28 +19,36 @@ class Tag(models.Model):
     def __str__(self):
         return self.tag
 
+# https://docs.djangoproject.com/en/2.2/ref/models/fields/#django.db.models.SET
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
-class BaseModel(models.Model):
+class BaseEntryModel(models.Model):
     created = models.DateTimeField(default=timezone.now)
-    tags = models.ManyToManyField(Tag, blank=True)
-    
+    body = models.TextField()
+    author = models.ForeignKey(
+            settings.AUTH_USER_MODEL,
+            on_delete=models.SET(get_sentinel_user),
+            default=None,
+    )
     class Meta:
         abstract = True
+        
 
-
-class Question(BaseModel):
+class Question(BaseEntryModel):
+    tags = models.ManyToManyField(Tag, blank=True)
     title = models.CharField(max_length=255)
-    body = models.TextField()
-    best_answer = models.CharField(max_length=255, blank=True)
+    # name 'Answer' is not defined
+    #best_answer = models.ForeignKey(Answer, on_delete=SET_NULL, related_name='question')
 
     def __str__(self):
         return self.title
 
 
-class Answer(BaseModel):
-    body = models.TextField()
-    to_question = models.ForeignKey('Question', related_name='answers')
-    created_by = models.ForeignKey(User, on_delete=SET_NULL, related_name='answers')
+class Answer(BaseEntryModel):
+    to_question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    is_best = models.BooleanField(default=False)
+    votes = models.SmallIntegerField(default=0)
     
     def __str__(self):
         return "{}'s answer to {}".format(self.created_by, self.to_question)
