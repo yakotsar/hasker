16c7905405295582d31f3c0af64e3b144c08e8aa
yakotsar <yakotsar@yandex.ru>
2019-06-03 16:24:17 +0300

    wtf with user models

--- hasker/models.py
+++ hasker/models.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 from django.db import models
 
@@ -9,8 +9,8 @@ def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 
-class Profile(User):
-    avatar = models.FileField(upload_to=user_directory_path)
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
 
 class Tag(models.Model):
@@ -27,7 +27,7 @@ class BaseEntryModel(models.Model):
     created = models.DateTimeField(default=timezone.now)
     body = models.TextField()
     author = models.ForeignKey(
-            settings.AUTH_USER_MODEL,
+            get_user_model(),
             on_delete=models.SET(get_sentinel_user),
             default=None,
     )
